a20f8c168505e8136a9af47599dbaed607a500d2
raniatze <xristina.rania.tze@gmail.com>
2025-06-06 19:41:03 +0200

    Add simple cuboid gbl

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import open3d as o3d
 
 # Example Open3D geometry (use your own)
-mesh = o3d.geometry.TriangleMesh.create_sphere()
-o3d.io.write_triangle_mesh("/home/raniatze/Documents/PhD/Research/PrITTI/pritti_page/static/models/myscene.glb", mesh)
+# mesh = o3d.geometry.TriangleMesh.create_sphere()
+# o3d.io.write_triangle_mesh("/home/raniatze/Documents/PhD/Research/PrITTI/pritti_page/static/models/myscene.glb", mesh)
+
+# Create a cuboid (box)
+mesh = o3d.geometry.TriangleMesh.create_box(width=1.0, height=2.0, depth=3.0)
+mesh.compute_vertex_normals()
+
+# Save it to a GLB file (note: this may not be supported depending on your Open3D version)
+o3d.io.write_triangle_mesh("cuboid.glb", mesh)
+
